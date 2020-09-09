7195fb76bad4006837a9523626ad656018309aa4
Joshua Lumley <Joshua.Lumley@pedersenread.co.nz>
2020-09-10 11:00:30 +1200

    Remove DragIES

---
+++ Source/_929_Bilt2020_PlaypenChild/Window20_DragIES.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _929_Bilt2020_PlaypenChild
+{
+    /// <summary>
+    /// Interaction logic for Window20_DragIES.xaml
+    /// </summary>
+    public partial class Window20_DragIES : Window
+    {
+        public Window20_DragIES()
+        {
+            InitializeComponent();
+        }
+    }
+}
